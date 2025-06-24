ad36970f8ebb6fd3f27447c47104ce03295db31b
Isha-ahsI <isha142005@gmail.com>
2025-06-24 14:48:25 +0530

    design offcanvas in navbar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //counting of metric
   const counters = document.querySelectorAll('.counter');
-
+  
   counters.forEach(counter => {
     const updateCount = () => {
       const target = +counter.getAttribute('data-target');
@@ -21,14 +22,26 @@ document.addEventListener("DOMContentLoaded", function () {
         observer.unobserve(counter);
       }
     });
-
     observer.observe(counter);
   });
+
+  //sticky navbar
+   const navbar = document.getElementById("mainNavbar");
+
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 10) {
+        navbar.classList.add("nav-scroll");
+      } else {
+        navbar.classList.remove("nav-scroll");
+      }
+    });
 });
 
 
+//gsap
 var tl=gsap.timeline()
 
+//navbar/hero section
 tl.from(".nav-item",{
     y:-40,
     opacity:0,
@@ -36,6 +49,7 @@ tl.from(".nav-item",{
     duration:0.5,
     stagger:0.1
 })
+
 tl.from(".mail-id,.hero-content",{
     x:-100,
     opacity:0,
@@ -43,6 +57,7 @@ tl.from(".mail-id,.hero-content",{
     duration:1,
     stagger:0.2
 },"0.5")
+
 tl.from(".profile-icon,.navbar-toggler,.hero-img",{
     x:100,
     opacity:0,
