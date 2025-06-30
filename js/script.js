a1ca0973b46eb185137d2f052d629807849dd42c
Isha-ahsI <isha142005@gmail.com>
2025-06-30 14:47:04 +0530

    gsap animation for footer section

--- js/script.js
+++ js/script.js
@@ -38,7 +38,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   //swiper for logos
   const swiper1 = new Swiper("#swiper1", {
-    slidesPerView: "auto",
     spaceBetween: 50,
     loop: true,
     speed: 3000,
@@ -49,17 +48,17 @@ document.addEventListener("DOMContentLoaded", function () {
     },
     allowTouchMove: false,
     breakpoints: {
-      0: { slidesPerView: 3 },
-      768: { slidesPerView: 5 },
-      992: { slidesPerView: 7 },
+      0: { slidesPerView: 3},
+      576:{slidesPerView: 4 },
+      768: { slidesPerView: 6 },
+      992: { slidesPerView: 8 },
     },
   });
 
   const swiper2 = new Swiper("#swiper2", {
-    slidesPerView: "auto",
     spaceBetween: 50,
     loop: true,
-    speed: 2500,
+    speed: 3000,
     autoplay: {
       delay: 0,
       reverseDirection: true,
@@ -67,16 +66,23 @@ document.addEventListener("DOMContentLoaded", function () {
     },
     allowTouchMove: false,
     breakpoints: {
-      0: { slidesPerView: 3 },
-      768: { slidesPerView: 5 },
-      992: { slidesPerView: 7 },
+      0: { slidesPerView: 3},
+      576:{slidesPerView: 4 },
+      768: { slidesPerView: 6 },
+      992: { slidesPerView: 8},
     },
   });
 });
 
 
 //gsap
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger,CustomEase, CustomBounce);
+CustomBounce.create("myBounce", {
+  strength: 0.6,
+  squash: 3,
+  squashID: "myBounce-squash",
+});
+
 var tl = gsap.timeline()
 
 //navbar/hero section
@@ -123,7 +129,7 @@ var tl2 = gsap.timeline({
    scrollTrigger: {
     trigger: ".service-section",
     scroller: "body",
-    markers: true,
+    markers: false,
     end: "top 30%",
     start: 'top 70%',
    }
@@ -176,3 +182,63 @@ tl4.from(".partner-section .partner-swiper", {
   opacity: 0,
   duration: 2
 })
+
+//footer
+var tl5 = gsap.timeline({
+  scrollTrigger: {
+    trigger: "footer",
+    scroller: "body",
+    markers: true,
+    start: "top 80%",
+    end: "top 80%",
+     toggleActions: "play none none reverse"
+  }
+})
+tl5.from(".footer-section .footer-brand-title",{
+  x: -100,
+  opacity: 0,
+  delay: 0.3,
+  duration: 1
+})
+
+tl5.from(".footer-section .footer-subscribe-box",{
+  x: 100,
+  opacity: 0,
+  delay: 0.3,
+  duration: 1
+},-0.1)
+
+tl5.from(".footer-section .footer-title,.footer-section .footer-nav-group .footer-nav-link,.footer-section .contact-info,.footer-section .location-info,.footer-section .mail-info",{
+  y:-30,
+  opacity: 0,
+  delay: 0.3,
+  duration: 1,
+  stagger: 0.1
+})
+tl5.from(".store-buttons-wrapper .store-button",{
+  opacity: 0,
+  delay: 0.1,
+  duration: 1,
+  stagger: 0.1
+})
+
+tl5.from(".footer-tagline .Copyright-section",{
+  y:30,
+  opacity: 0,
+  delay: 0.1,
+  duration: 1
+})
+
+tl5.from(".footer-tagline .footer-social-icons .social-media-icon", {
+   duration: 2, 
+   y: -100, 
+   ease: "myBounce" 
+});
+
+tl5.to(".footer-tagline .footer-social-icons .social-media-icon", {
+  duration: 2,
+  scaleX: 1.4,
+  scaleY: 0.6,
+  transformOrigin: "center bottom",
+  ease: "myBounce-squash",
+});
